619f1b85bfe0d167d2a626536e0e2a10aa3910b7
Thanh Tran <69281655+draytht@users.noreply.github.com>
2023-12-12 00:10:03 -0600

    Add files via upload

---
+++ src/components/components/canvas/index.js
@@ -0,0 +1,16 @@
+import EarthCanvas from "./Earth";
+import BallCanvas from "./Ball";
+import ComputersCanvas from "./Computers";
+import StarsCanvas from "./Stars";
+
+export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
+// src/components/index.js
+export { default as About } from './About';
+export { default as Contact } from './Contact';
+export { default as Experience } from './Experience';
+export { default as Feedbacks } from './Feedbacks';
+export { default as Hero } from './Hero';
+export { default as Navbar } from './Navbar';
+export { default as Tech } from './Tech';
+export { default as Works } from './Works';
+export { default as StarsCanvas } from './StarsCanvas';
---
+++ src/components/components/index.js
@@ -0,0 +1,34 @@
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import About from './About';
+import Tech from './Tech';
+import Experience from './Experience';
+import Works from './Works';
+import Feedbacks from './Feedbacks';
+import Contact from './Contact';
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
+}
+// src/components/index.js
+export { default as About } from './About';
+export { default as Contact } from './Contact';
+export { default as Experience } from './Experience';
+export { default as Feedbacks } from './Feedbacks';
+export { default as Hero } from './Hero';
+export { default as Navbar } from './Navbar';
+export { default as Tech } from './Tech';
+export { default as Works } from './Works';
+export { default as StarsCanvas } from './StarsCanvas';
